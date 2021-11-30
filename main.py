51cf5337acca96de296c88f24f5fd9b26628e4a7
Hubert Wawszczak <hubert.t.wawszczak@gmail.com>
2021-11-30 22:59:23 +0100

    final

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from modules.demo import *
-
+import config
 
 def main():
     if __name__ == '__main__':
         obj = demo()
 
-        obj.GetResponseWeather(input('Podaj nazwe miasta '))
+        obj.GetResponseWeather(input('Podaj nazwe miasta '),config.x_rapidapi_key)
 
 
 main()
--- modules/demo.py
+++ modules/demo.py
@@ -7,25 +7,28 @@ class demo:
         self.a = 4
         self.b = 5
 
-    def GetResponseWeather(self,cityName):
+    def GetResponseWeather(self,cityName,rapidapi_key):
         url = "https://community-open-weather-map.p.rapidapi.com/weather"
 
         querystring = {"q":cityName,"units":"metric"}
 
         headers = {
             'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
-            'x-rapidapi-key': "7501b24147mshd595fc8f2e5ac13p19fdf6jsnc5d0718ed300"
+            'x-rapidapi-key': rapidapi_key
         }
+        try:
+         response = requests.request("GET", url, headers=headers, params=querystring)
+         with open('response.json', 'wb') as outf:
+             outf.write(response.content)
 
-        response = requests.request("GET", url, headers=headers, params=querystring)
+         data = json.loads(response.content)
+         print("Miasto: " + data["name"])
+         print("Temperatura: " + str(data["main"]["temp"]) + u'\xb0' + "C")
+         print("Ciśnienie : " + str(data["main"]["pressure"]) + " hPa")
+         print("Pogoda: {}".format(data["weather"][0]["main"]))
+        except BaseException:
+            print("złe miasto ")
 
-        with open('response.json', 'wb') as outf:
-            outf.write(response.content)
-
-        data = json.loads(response.content)
-        print("Miasto: " + data["name"])
-        print("Temperatura: " + str(data["main"]["temp"]) + u'\xb0'+"C")
-        print("Ciśnienie : " + str(data["main"]["pressure"]) + " hPa")
 
 
 
